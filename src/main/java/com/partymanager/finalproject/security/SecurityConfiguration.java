995987597d49efcd9f62e51e2ea4c2a96e502e79
BHammock33 <105878596+BHammock33@users.noreply.github.com>
2023-03-12 20:54:41 -0400

    3.12.23 PM
    transferring security to FilterChain

--- src/main/java/com/partymanager/finalproject/security/SecurityConfiguration.java
+++ src/main/java/com/partymanager/finalproject/security/SecurityConfiguration.java
@@ -3,55 +3,58 @@ package com.partymanager.finalproject.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.partymanager.finalproject.service.UserDetailsServiceImpl;
 
-@SuppressWarnings("deprecation")
-@Configuration
 
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration{
 
 	@Autowired
 	private UserDetailsServiceImpl userDetailsServiceImpl;
 	
-	
 	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((authz) -> authz
+				.antMatchers("/admin/**").hasRole("ADMIN")
+				.antMatchers("/user/**").hasRole("USER")
+				.anyRequest().authenticated());
+		return http.build();
 	}
 	
-	@Override
-	protected void configure(AuthenticationManagerBuilder authentication) throws Exception {
-		authentication
-			.userDetailsService(userDetailsServiceImpl)
-			.passwordEncoder(passwordEncoder());
-	}
-	
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		http.csrf().disable().authorizeHttpRequests()
-		.antMatchers("/admin/").hasAnyRole("ADMIN")
-		.antMatchers("/register").permitAll()
-		.antMatchers("/css/**").permitAll()
-		.antMatchers("/images/**").permitAll()
-		.anyRequest().authenticated().and()
-		.formLogin()
-		.loginPage("/login")
-		.defaultSuccessUrl("/user", true).permitAll();
-	}
-	
-	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
+//	
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder authentication) throws Exception {
+//		authentication
+//			.userDetailsService(userDetailsServiceImpl)
+//			.passwordEncoder(passwordEncoder());
+//	}
+//	
+//	
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception{
+//		http.csrf().disable().authorizeHttpRequests()
+//		.antMatchers("/admin/").hasAnyRole("ADMIN")
+//		.antMatchers("/register").permitAll()
+//		.antMatchers("/css/**").permitAll()
+//		.antMatchers("/images/**").permitAll()
+//		.anyRequest().authenticated().and()
+//		.formLogin()
+//		.loginPage("/login")
+//		.defaultSuccessUrl("/user", true).permitAll();
+//	}
+//	
+//	@Bean
+//	@Override
+//	public AuthenticationManager authenticationManagerBean() throws Exception {
+//		return super.authenticationManagerBean();
+//	}
 }
